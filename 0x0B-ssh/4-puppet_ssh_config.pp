2b91f1fd1a3ae87176c08736dd24a9783a85c7be
jfangwang <qbs1864@gmail.com>
2021-04-04 20:03:47 +0000

    removed ensure

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,9 @@
 # Create a file holberton
 
 file_line { 'Turn off passwd auth':
-    ensure => 'present',
     path   => '/etc/ssh/ssh_config',
-    match  => 'PasswordAuthentication'
     line   => 'PasswordAuthentication no'
     replace=> true,
 file_line { 'Declare identity file':
-    ensure => 'present',
     path   => '/etc/ssh/ssh_config',
     line   => 'IdentityFile ~/.ssh/holberton'
