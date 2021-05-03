43ed83b06ea2eca8a9bb497bf84ffbaae2596b19
jfangwang <qbs1864@gmail.com>
2021-05-03 17:29:51 +0000

    0 done

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        user_id = str(argv[1])
+        url = "https://jsonplaceholder.typicode.com/users"
+        r = requests.get(url).json()
+        name = "Willy"
+        task_count = 0
+        task_completed = 0
+        # Get the user name with user id
+        for item in r:
+            if str(item['id']) == user_id:
+                name = str(item['name'])
+        url = "https://jsonplaceholder.typicode.com/todos"
+        r = requests.get(url).json()
+        for item in r:
+            if str(item['userId']) == user_id:
+                task_count += 1
+            if str(item['userId']) == user_id and str(item['completed']) == 'True':
+                task_completed += 1
+        print("Employee {} is done with tasks({}/{}):".format(name, task_completed, task_count))
+        for item in r:
+            if str(item['userId']) == user_id and str(item['completed']) == 'True':
+                print("\t {}".format(item['title']))
