d6e5a8c988224c12fc696ba324d1e6b72a8369eb
jfangwang <qbs1864@gmail.com>
2021-05-03 18:26:19 +0000

    2 done

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import requests
 from sys import argv
-import csv
+import json
 
 if __name__ == "__main__":
     if len(argv) == 2:
@@ -15,17 +15,19 @@ if __name__ == "__main__":
         for item in r:
             if str(item['id']) == user_id:
                 user_name = str(item['username'])
+        # Get Data
         url = "https://jsonplaceholder.typicode.com/todos"
         r = requests.get(url).json()
-        # Count tasks
-        for item in r:
-            if str(item['userId']) == user_id:
-                task_count += 1
-            if str(item['userId']) == user_id and str(item['completed']) == 'True':
-                task_completed += 1
-        with open('2.csv', mode='w') as employee_file:
-            employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-            for item in r:
-                if str(item['userId']) == user_id:
-                    #"2","Antonette","False","suscipit repellat esse quibusdam voluptatem incidunt"
-                    employee_writer.writerow([user_id, user_name, str(item['completed']), str(item['title'])])
+        mega_dict = {}
+        temp_dict = {}
+        willy_list = []
+        for d in r:
+            # Titles, Completed, Username
+            temp_dict = {}
+            temp_dict['task'] = d['title']
+            temp_dict['username'] = user_name
+            temp_dict['completed'] = d['completed']
+            willy_list.append(temp_dict)
+        mega_dict[user_id] = willy_list
+        with open('{}.json'.format(user_id), 'w') as json_file:
+            json.dump(mega_dict, json_file)
