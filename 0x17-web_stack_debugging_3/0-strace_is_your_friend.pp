1a0225f4b77525c01e772eb9f3fce6d3713a1508
jfangwang <qbs1864@gmail.com>
2021-06-25 19:56:37 +0000

    puppet lint verified

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Mispelled the extension...should be php instead of phpp
 exec { 'fix extension':
-	command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	provider => shell,
+    command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    provider => shell,
 }
