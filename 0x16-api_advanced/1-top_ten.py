6d16c29fd6998d286e20481d9e5efd78219d4f0d
jfangwang <qbs1864@gmail.com>
2021-06-26 22:09:10 +0000

    pep8 and finalized

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Querying the Reddit API"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Returns Number of Subs"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    h = {
+        'User-Agent': 'Python/1.0(Holberton Project)'
+    }
+    req = requests.get(url, headers=h)
+    count = 0
+
+    posts = req.json()['data']['children']
+    if posts == []:
+        print("None")
+    else:
+        for item in posts:
+            if count < 10:
+                count = count + 1
+                print(item['data']['title'])
