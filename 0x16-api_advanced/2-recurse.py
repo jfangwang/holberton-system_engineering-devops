346fbb0219566f61f0a836bcecca4df9420fe9bf
jfangwang <qbs1864@gmail.com>
2021-06-26 22:44:50 +0000

    pep8 and working

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Querying the Reddit API"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after='null'):
+    """Returns Number of Subs"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    h = {
+        'User-Agent': 'Python/1.0(Holberton Project)'
+    }
+    after = {
+        "after": after
+    }
+    req = requests.get(url, headers=h, allow_redirects=False, params=after)
+    try:
+        after = req.json()['data']['after']
+        posts = req.json()['data']['children']
+    except:
+        return None
+
+    for item in posts:
+        hot_list.append(item['data']['title'])
+    print(len(hot_list))
+    if after not in ["NULL", 'null', None, "None"]:
+        return recurse(subreddit, hot_list, after)
+    else:
+        return hot_list
